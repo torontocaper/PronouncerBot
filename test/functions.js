027aef99906c340b84db9f1ab2feade5f5cdafa2
Adam <torontocaper@gmail.com>
2022-04-18 14:41:53 -0400

    removed functions.js

--- test/functions.js
+++
@@ -1,9 +0,0 @@
-function storeData() {
-  let docProps = PropertiesService.getDocumentProperties();
-  docProps.setProperty("documentID", "1HbkohjN85eOX_ioYAQUBvWnkl0X79qgycIKbjQJmUYc")
-}
-
-function readData() {
-  let docProps = PropertiesService.getDocumentProperties();
-  Logger.log(docProps.getProperty("documentID"))
-}
