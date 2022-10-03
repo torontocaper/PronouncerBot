d7da7a0b693e796437d934791707bf6eac4fb27f
Adam <torontocaper@gmail.com>
2022-10-03 10:57:14 -0400

    added comment

--- functions3_onEdit.js
+++ functions3_onEdit.js
@@ -1,5 +1,6 @@
 function onEdit() {
     let spreadsheet = SpreadsheetApp.getActive();
+    //sort by "Title"
     spreadsheet.getRange('B:B').activate();
     spreadsheet.getActiveSheet().sort(2, true);
   }  
