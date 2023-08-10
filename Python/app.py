9668aa4ae8aeee902eef7d7cea14f8d40e560e5a
Adam <torontocaper@gmail.com>
2023-08-10 08:48:05 -0400

    double commit?

--- Python/app.py
+++ Python/app.py
@@ -10,7 +10,7 @@ from slack_sdk import WebClient
 load_dotenv()
 bot_token = os.getenv("BOT_USER_TOKEN")
 client = WebClient(token=bot_token)
-headers = {"Authorization": "Bearer " + bot_token}
+# headers = {"Authorization": "Bearer " + bot_token}
 
 # Load JSON data from file
 with open("Python/pronouncers.json", "r") as json_file:
@@ -18,7 +18,6 @@ with open("Python/pronouncers.json", "r") as json_file:
 
 app = Flask(__name__)
 
-challenge_key = ""
 event_cache = []
 print(f"{datetime.now()}: Current event cache: {event_cache}.")
 
@@ -29,26 +28,24 @@ def handle_slack_event():
     print(f"{datetime.now()}: Request received. Starting event handler.")
 
     if "challenge" in request_data:
-        print(f"{datetime.now()}: This is an authentication challenge from Slack. The challenge key is {challenge_key}")
         challenge_key = request_data["challenge"]
+        print(f"{datetime.now()}: This is an authentication challenge from Slack. The challenge key is {challenge_key}")
         return challenge_key
     
     else:
-        event_context = request_data["event_context"]
-        event_time = request_data["event_time"]
-        print(f"{datetime.now()}: Outer event info: Context: {event_context}; Event Time: {event_time}")
+        # event_context = request_data["event_context"]
+        # event_time = request_data["event_time"]
+        # print(f"{datetime.now()}: Outer event info: Context: {event_context}; Event Time: {event_time}")
 
         event_id = request_data["event_id"]
-
         event_data = request_data["event"]
         user_id = event_data["user"]
         reaction_type = event_data["reaction"]
-        
         item_data = event_data["item"]
         channel_id = item_data["channel"]
         timestamp = item_data["ts"]      
     
-        print(f"{datetime.now()}: Inner event info: Event: {event_id}; User: {user_id}; Reaction: {reaction_type}; Channel: {channel_id}; Timestamp: {timestamp}.")
+        # print(f"{datetime.now()}: Inner event info: Event: {event_id}; User: {user_id}; Reaction: {reaction_type}; Channel: {channel_id}; Timestamp: {timestamp}.")
 
         if event_id in event_cache:
             print(f"{datetime.now()}: Event {event_id} has already been processed. Exiting event handler.")
@@ -58,7 +55,7 @@ def handle_slack_event():
             print(f"{datetime.now()}: Event {event_id} has not been processed. Starting event processing.")
             event_cache.append(event_id)
 
-            # handle emoji reactions
+            # if the emoji is anything other than :pronounce:, bail
             if reaction_type != "pronounce":
                 print(f"{datetime.now()}: Reaction was of type :{reaction_type}:. Exiting event handler.")
                 return "OK"
@@ -69,21 +66,29 @@ def handle_slack_event():
                 )
                 messages = conversation.get("messages")
                 message_text = messages[0]["text"]
-                print(f"{datetime.now()}: Pronouncer requested for \"{message_text}\".")
+                client.chat_postMessage(channel=channel_id, thread_ts=timestamp, text=f"Hey <@{user_id}>, thanks for calling PronouncerBot. I'll look for a pronouncer for {message_text}.")
 
-                # Search for a specific name
-                search_text = message_text
+                # search the database for the message text
                 matches = []
-
                 for entry in pronouncer_data:
-                    if search_text.lower() in entry["Title"].lower():
+                    if message_text.lower() in entry["Title"].lower():
                        matches.append(entry)
 
+                # if no matches are found
                 if len(matches) == 0:
                     client.chat_postMessage(channel=channel_id, thread_ts=timestamp, text=f"No matches. Sorry!")
 
+                
+                # if one match is found
+                elif len(matches) == 1:
+                    client.chat_postMessage(channel=channel_id, thread_ts=timestamp, text=f"I found a match!")
+                    title = matches[0]["Title"]
+                    pronouncer = matches[0]["Pronouncer"]
+                    client.chat_postMessage(channel=channel_id, thread_ts=timestamp, text=f"{title} - {pronouncer}.")
+
+                # if several matches are found
                 else:
-                    client.chat_postMessage(channel=channel_id, thread_ts=timestamp, text=f"We found {len(matches)} matches!") 
+                    client.chat_postMessage(channel=channel_id, thread_ts=timestamp, text=f"I found {len(matches)} potential matches!") 
                     for match in matches:
                         title = match["Title"]
                         pronouncer = match["Pronouncer"]
