dfee0aeaac6dc4ea2f32e9072f8485d83b98a7d1
Adam <torontocaper@gmail.com>
2023-08-21 16:03:53 -0400

    cleaned up a few things

--- Python/app.py
+++ Python/app.py
@@ -38,10 +38,6 @@ def handle_slack_event():
         return challenge_key
     
     else:
-        # event_context = request_data["event_context"]
-        # event_time = request_data["event_time"]
-        # print(f"{datetime.now()}: Outer event info: Context: {event_context}; Event Time: {event_time}")
-
         event_id = request_data["event_id"]
         event_data = request_data["event"]
         user_id = event_data["user"]
@@ -50,8 +46,6 @@ def handle_slack_event():
         channel_id = item_data["channel"]
         timestamp = item_data["ts"]      
     
-        # print(f"{datetime.now()}: Inner event info: Event: {event_id}; User: {user_id}; Reaction: {reaction_type}; Channel: {channel_id}; Timestamp: {timestamp}.")
-
         if event_id in event_cache:
             print(f"{datetime.now()}: Event {event_id} has already been processed. Exiting event handler.")
             return "OK"
@@ -149,8 +143,6 @@ def handle_payload(request_data):
                     print(f"{title} ({description}) - {pronouncer}")
                 else:
                     print(f"{title} - {pronouncer}")
-
-
 
         return "OK"
 
